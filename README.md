5d29ec01f7547bb071aa585879ba7a2bf4a39026
Amit Panda <amitpanda007@gmail.com>
2022-04-24 10:20:34 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Python Flask API for elastic search
+
+#Requirements
+1. Install below python libs
+eg. pip install <lib_name>
+
+* elasticsearch
+* flask
+* flask_cors
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from search_api import search_with_name
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, From Search Server!</p>"
+
+
+@app.route("/search")
+def search():
+    partial_search = request.args.get('partialSearch')
+    return jsonify(search_with_name(partial_search))
